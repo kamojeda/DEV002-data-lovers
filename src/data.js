a1153c0d826f53d8ea9432e0c33daad6b1c744f6
beresdev <beresdev@gmail.com>
2022-11-24 14:25:11 -0600

    Se elimina el manejo de errores de las funciones filterAScendent() y filterDescendent() de data.js

--- src/data.js
+++ src/data.js
@@ -32,27 +32,15 @@ export const filterCompare = (array, cName) => {
 }
 
 export const filterAscendent = (array) => {
-  if (array === undefined) {
-    throw new TypeError('data is undefined')
-  } else if (array === null) {
-    throw new TypeError('data is null')
-  } else {
   let ascendent = array.sort(function(a, b) {
     return((a.name < b.name)? -1: ((a.name > b.name)? 1: 0));
     })
     return ascendent;
-  }
 }
 
 export const filterDescendent = (array) => {
-  if (array === undefined) {
-    throw new TypeError('data is undefined')
-  } else if (array === null) {
-    throw new TypeError('data is null')
-  } else {
   let descendent = array.sort(function(a, b) {
     return((a.name < b.name)? 1: ((a.name > b.name)? -1: 0));
     })
     return descendent;
-  }
 }
